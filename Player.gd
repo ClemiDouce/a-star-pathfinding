39f9d09d323e17044e77298eba3f5996ece612c4
Mathieu <dupycournau@gmail.com>
2022-01-01 23:46:29 +0100

    add perpetual movement

--- Player.gd
+++ Player.gd
@@ -9,18 +9,19 @@ var path_index = 0
 var step := true
 var stop := false
 var moving := false
+var path_offset := 10
 
 signal stopped(pos)
 
-
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	self.connect("stopped", self, "on_self_stopped")
 
 func new_path(new_path: PoolVector2Array, move_by_step=true):
 	if new_path.size() > 0:
 		self.path = new_path
 		path_index = 0
 		step = move_by_step
+		map.draw_path_line(self.path)
 	
 func move():
 	stop = false
@@ -44,14 +45,33 @@ func move():
 			path_index += 1
 			tween.start()
 			yield(tween, "tween_completed")
+		print('Fin du chemin')
 	moving = false
 	emit_signal("stopped", self.global_position)
-	print('Fin du chemin')
+	
 	
 
-func stop():
+func pause():
 	self.stop = true
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_down"):
-		stop()
+		pause()
+		
+func check_path():
+	if path.size() == 0:
+		return
+		
+	for point in path:
+		if map.get_cellv(point) == 2:
+			return false
+	return true
+		
+func random_offset():
+	var x = rand_range(-path_offset, path_offset)
+	var y = rand_range(-path_offset, path_offset)
+	return Vector2(x,y)
+
+func on_self_stopped(pos):
+	new_path(map.get_random_destination(pos), false)
+	self.move()
--- RealMap.gd
+++ RealMap.gd
@@ -3,8 +3,12 @@ extends Astar_Path
 onready var player := $Player
 onready var line := $Line2D
 
+var offset := 4
+
 func _ready() -> void:
 	player.map = self
+	player.new_path(get_random_destination(player.global_position), false)
+	player.move()
 
 func _input(event: InputEvent) -> void:
 	if (event is InputEventMouseButton and event.is_pressed()):
@@ -15,28 +19,25 @@ func _input(event: InputEvent) -> void:
 				var path = search_path(player.global_position, mouse_pos)
 				if path:
 					player.new_path(path)
-#					self.set_cellv(path[-1], 1)
-					draw_path_line(path)
 				
 		# Placer obstacle
 		elif event.button_index == BUTTON_RIGHT:
 			var mouse_pos = get_global_mouse_position()
 			if used_cells.has(world_to_map(mouse_pos)): # Check si case cliquée existe
+				print("State of Player", player.moving)
 				if !player.moving:
 					var path = search_path(player.global_position, mouse_pos)
 					if path:
 						player.new_path(path, false)
 	#					self.set_cellv(path[-1], 1)
-						draw_path_line(path)
 						player.move()
 				else:
-					player.stop()
+					player.pause()
 					yield(player, "stopped")
 					var path = search_path(player.global_position, mouse_pos)
 					if path:
 						player.new_path(path, false)
 	#					self.set_cellv(path[-1], 1)
-						draw_path_line(path)
 						player.move()
 		
 		# Arreter le joueur
@@ -48,11 +49,27 @@ func search_path(start: Vector2, end:Vector2) -> PoolVector2Array:
 	var new_end = world_to_map(end)
 	return _get_path(new_start, new_end)
 
+func get_cell_middle(cell: Vector2):
+	var half_cell_size = self.cell_size / 2
+	return map_to_world(cell) + half_cell_size
+
+func get_random_cell():
+	var index = rand_range(0, used_cells.size())
+	var random_cell = used_cells[index]
+	while self.get_cellv(used_cells[index]) == 2:
+		index = rand_range(0, used_cells.size())
+		random_cell = used_cells[index]
+	return random_cell
+	
+func get_random_destination(start: Vector2):
+	var rand_cell = get_random_cell()
+	var start_cell = world_to_map(start)
+	while rand_cell == start_cell:
+		rand_cell = get_random_cell()
+	var path = _get_path(start_cell, rand_cell)
+	return path
+
 func draw_path_line(path: PoolVector2Array):
 	line.clear_points()
 	for point in path:
 		line.add_point(get_cell_middle(point))
-
-func get_cell_middle(cell: Vector2):
-	var half_cell_size = self.cell_size / 2
-	return map_to_world(cell) + half_cell_size
